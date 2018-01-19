dd2608699ec235230d827441a33489f29ad4c532
utv <k9amata@gmail.com>
2018-01-19 09:00:37 -0800

    refactor

--- src/App.js
+++ src/App.js
@@ -75,50 +75,6 @@ class App extends Component {
     )
   }
 
-  renderTable(side) {
-    if (this.state.weights.length === 0) return
-    const NUM_COLUMNS = 3
-    /* const theWeights = side === this.LEFT
-    ? this.state.weights.filter((w) => w > 0)
-    : this.state.weights.filter((w) => w < 0) */
-    const rows = this.state.weights.reduce((acc, val, idx) => {
-      const value =
-        val > 0 && side === this.LEFT
-          ? val
-          : val < 0 && side === this.RIGHT
-            ? val * -1
-            : ''
-      const td = (
-        <td key={idx}>
-          <Circle
-            bgColor={this.LEFT === side ? 'crimson' : 'darkcyan'}
-            weight={value}
-          >
-            {value}
-          </Circle>
-        </td>
-      )
-
-      if (idx % NUM_COLUMNS !== 0) {
-        const row = acc.pop()
-        row.push(td)
-        acc.push(row)
-      } else {
-        acc.push([td])
-      }
-      return acc
-    }, [[]])
-
-    console.log('rows', rows)
-    return (
-      <table>
-        <tbody>
-          {rows.map((row, rid) => <tr key={rid}>{row}</tr>)}
-        </tbody>
-      </table>
-    )
-  }
-
   render() {
     const bgColor = (weight, side) => weight > 0 && side === 'L'
       ? 'crimson'
